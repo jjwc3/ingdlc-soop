25a9a7f3703cbacc2bc8e2b5fda9cc768a1de8a8
jjwc3 <jjwjjw2007@naver.com>
2025-12-24 22:11:29 +0900

    251224

--- src/popup/App.svelte
+++ src/popup/App.svelte
@@ -1,37 +1,32 @@
-<script lang='ts'>
-  import CrxLogo from '@/assets/crx.svg'
-  import svelteLogo from '@/assets/svelte.svg'
-  import viteLogo from '@/assets/vite.svg'
-  import HelloWorld from '@/components/HelloWorld.svelte'
-</script>
+<head>
+  <meta charset="UTF-8">
+  <title>INGDLC for SOOP</title>
+  <style>
+    body::-webkit-scrollbar {
+      width: 10px;
+    }
+    body::-webkit-scrollbar-thumb {
+      background-color: #555;
+      border-radius: 10px;
+      background-clip: padding-box;
+      border:1px solid transparent;
+    }
+    body::-webkit-scrollbar-track {
+      background-color: #999;
+      border-radius: 10px;
+    }
+  </style>
+</head>
 
-<div>
-  <a href='https://vite.dev' target='_blank'>
-    <img src={viteLogo} class='logo' alt='Vite logo'>
-  </a>
-  <a href='https://svelte.dev' target='_blank'>
-    <img src={svelteLogo} class='logo svelte' alt='Svelte logo'>
-  </a>
-  <a href='https://crxjs.dev/vite-plugin' target='_blank'>
-    <img src={CrxLogo} class='logo crx' alt='crx logo'>
-  </a>
+<div style="min-width:400px;" class="text-slate-900 bg-slate-50 px-2 py-4">
+  <header>
+    <h1 class="text-2xl font-bold text-center">
+      INGDLC for SOOP
+    </h1>
+
+    <div class="text-center text-xs text-slate-500">
+      Ver.<span id="version-current">0.0</span>
+    </div>
+
+  </header>
 </div>
-<HelloWorld msg='Vite + Svelte + CRXJS' />
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .logo.crx:hover {
-    filter: drop-shadow(0 0 2em #f2bae4aa);
-  }
-</style>
